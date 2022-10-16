104d1639770f25f75087697c61ea277d30200d6f
Laura Oprisan <isbasoiulaura@yahoo.com>
2022-10-16 21:58:08 +0300

    added exercise

---
+++ exam-scores/README.md
@@ -0,0 +1,3 @@
+#Task of the exercise
+
+"Integer array. Make a program in which you create a 10 element array for exam scores. Use for- loop to ask 10 exam scores and input them to the array. After this the scores are printed onto the screen and also shown the total score (all scores added together), and also average score."
---
+++ exam-scores/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	const int SIZE = 10;
+	float sum = 0, average, scores[SIZE];
+	int i;
+
+	cout << "Enter the exam score: \n";  //score input section
+	for (i=0;i<SIZE;i++)
+		cin >> scores[i];
+	cout << "The exam scores are: ";    //printing score section
+	for (i = 0; i < SIZE; i++)
+		cout << scores[i] << " ";
+	for (i = 0; i < SIZE; i++)	// calculating sum
+		sum += scores[i];
+	cout <<endl<< "The sum of of all scores are: "<<sum<<endl; //printing sum
+	average = sum / SIZE; //calculating average
+	cout << "The average score is: " << average; //printing average
+	return 0;
+
+}
